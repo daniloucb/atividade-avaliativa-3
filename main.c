86251a305012c55a19c2c51356bf242b49e711ab
daniloucb <148488541+daniloucb@users.noreply.github.com>
2023-12-06 14:26:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -47,15 +47,15 @@ Produto *cadastrar(Produto *lista)
   printf("\nDigite o código do produto: ");
   scanf("%d", &novoProduto->codigo);
 
-  // clearBuffer();
-  // printf("\nDigite a descrição do produto: ");
-  // fgets(novoProduto->descricao, 50, stdin);
+  clearBuffer();
+  printf("\nDigite a descrição do produto: ");
+  fgets(novoProduto->descricao, 50, stdin);
 
-  // printf("\nDigite a quantidade do produto: ");
-  // scanf("%d", &novoProduto->quantidade);
+  printf("\nDigite a quantidade do produto: ");
+  scanf("%d", &novoProduto->quantidade);
 
-  // printf("\nDigite a valor do produto: ");
-  // scanf("%d", &novoProduto->valor);
+  printf("\nDigite a valor do produto: ");
+  scanf("%d", &novoProduto->valor);
 
   if(lista == NULL)
     return novoProduto;
